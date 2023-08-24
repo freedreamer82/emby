c5df2706c9850f37fc2fab1758aed0b502ed36f7
Marco Garzola <zolamarco@gmail.com>
2023-08-24 12:28:29 +0200

    -add macro to get all subdirs in cmake - fix includes

--- emby/PlatformSpecific/X86_Unix/emby-platform.cmake
+++ emby/PlatformSpecific/X86_Unix/emby-platform.cmake
@@ -2,16 +2,17 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_VERSION 1)
 cmake_minimum_required(VERSION 3.17)
 
-message(STATUS "include cmake from: " ${EMBY_PLATFORM})
+message(STATUS "including cmake from: " ${EMBY_PLATFORM})
 
 
 set(ENV{EMBY_CROSS_BUILD_ARCH} "X86")
 add_compile_definitions(EMBY_BUILD_X86)
 
-
-include_directories(${EMBY_FOLDER}/PlatformSpecific/${EMBY_PLATFORM}/*
-                    ${EMBY_FOLDER}/PlatformSpecific/${EMBY_PLATFORM}/*/*
-)
+#include all sbdirectories
+SUBDIRLIST(SUBDIRS "${EMBY_FOLDER}/PlatformSpecific/${EMBY_PLATFORM}/")
+foreach(subdir ${SUBDIRS})
+    include_directories(${EMBY_FOLDER}/PlatformSpecific/${EMBY_PLATFORM}/${subdir})
+endforeach()
 
 file(GLOB_RECURSE EMBY_PLAT_SOURCES
 "${EMBY_FOLDER}/PlatformSpecific/${EMBY_PLATFORM}/*/*.c"   
