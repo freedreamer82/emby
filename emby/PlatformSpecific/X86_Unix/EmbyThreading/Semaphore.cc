c26de3e231872da23f8c3d71fe66cfd4511ca499
Marco Garzola <zolamarco@gmail.com>
2023-08-24 13:32:11 +0200

    -fix apple semaphore

--- emby/PlatformSpecific/X86_Unix/EmbyThreading/Semaphore.cc
+++ emby/PlatformSpecific/X86_Unix/EmbyThreading/Semaphore.cc
@@ -1,59 +1,73 @@
 #include <EmbyThreading/Semaphore.hh>
 #include <EmbySystem/System.hh>
 #include <errno.h>
+#include <semaphore.h>
 #include <time.h>
 
 #ifdef __APPLE__
 #include "pthread.h"
+typedef struct
+{
+    pthread_mutex_t count_lock;
+    pthread_cond_t  count_bump;
+    unsigned count;
+}
+bosal_sem_t;
 
 int sem_init(sem_t *psem, int flags, unsigned count)
 {
+    bosal_sem_t *pnewsem;
     int result;
 
-    if (psem == NULL)
+    pnewsem = (bosal_sem_t *)malloc(sizeof(bosal_sem_t));
+    if (! pnewsem)
     {
         return -1;
     }
-    result = pthread_mutex_init(&psem->count_lock, NULL);
-    if (result != 0)
+    result = pthread_mutex_init(&pnewsem->count_lock, NULL);
+    if (result)
     {
+        free(pnewsem);
         return result;
     }
-    result = pthread_cond_init(&psem->count_bump, NULL);
-    if (result != 0)
+    result = pthread_cond_init(&pnewsem->count_bump, NULL);
+    if (result)
     {
-        pthread_mutex_destroy(&psem->count_lock);
+        pthread_mutex_destroy(&pnewsem->count_lock);
+        free(pnewsem);
         return result;
     }
-    psem->count = count;
+    pnewsem->count = count;
+    *psem = (uint64_t)pnewsem;
     return 0;
 }
 
 int sem_destroy(sem_t *psem)
 {
-    sem_t *poldsem;
+    bosal_sem_t *poldsem;
 
-    if (!psem)
+    if (! psem)
     {
         return EINVAL;
     }
-    poldsem = (sem_t *)psem;
+    poldsem = (bosal_sem_t *)*psem;
 
     pthread_mutex_destroy(&poldsem->count_lock);
     pthread_cond_destroy(&poldsem->count_bump);
+    free(poldsem);
     return 0;
 }
 
 int sem_post(sem_t *psem)
 {
-    sem_t *pxsem;
+     bosal_sem_t *pxsem;
     int result, xresult;
 
-    if (!psem)
+    if (! psem)
     {
         return EINVAL;
     }
-    pxsem = (sem_t *)psem;
+    pxsem = (bosal_sem_t *)*psem;
 
     result = pthread_mutex_lock(&pxsem->count_lock);
     if (result)
@@ -74,19 +88,19 @@ int sem_post(sem_t *psem)
         errno = xresult;
         return -1;
     }
-    return 0;
+     return 0;
 }
 
 int sem_trywait(sem_t *psem)
 {
-    sem_t *pxsem;
+    bosal_sem_t *pxsem;
     int result, xresult;
 
-    if (!psem)
+    if (! psem)
     {
         return EINVAL;
     }
-    pxsem = (sem_t *)psem;
+    pxsem = (bosal_sem_t *)*psem;
 
     result = pthread_mutex_lock(&pxsem->count_lock);
     if (result)
@@ -118,14 +132,14 @@ int sem_trywait(sem_t *psem)
 
 int sem_wait(sem_t *psem)
 {
-    sem_t *pxsem;
+    bosal_sem_t *pxsem;
     int result, xresult;
 
-    if (!psem)
+    if (! psem)
     {
         return EINVAL;
     }
-    pxsem = (sem_t *)psem;
+    pxsem = (bosal_sem_t *)*psem;
 
     result = pthread_mutex_lock(&pxsem->count_lock);
     if (result)
@@ -138,7 +152,7 @@ int sem_wait(sem_t *psem)
     {
         xresult = pthread_cond_wait(&pxsem->count_bump, &pxsem->count_lock);
     }
-    if (!xresult)
+    if (! xresult)
     {
         if (pxsem->count > 0)
         {
@@ -160,14 +174,14 @@ int sem_wait(sem_t *psem)
 
 int sem_timedwait(sem_t *psem, const struct timespec *abstim)
 {
-    sem_t *pxsem;
+    bosal_sem_t *pxsem;
     int result, xresult;
 
-    if (psem == NULL)
+    if (! psem)
     {
         return EINVAL;
     }
-    pxsem = (sem_t *)psem;
+    pxsem = (bosal_sem_t *)*psem;
 
     result = pthread_mutex_lock(&pxsem->count_lock);
     if (result)
@@ -178,10 +192,9 @@ int sem_timedwait(sem_t *psem, const struct timespec *abstim)
 
     if (pxsem->count == 0)
     {
-        xresult =
-            pthread_cond_timedwait(&pxsem->count_bump, &pxsem->count_lock, abstim);
+        xresult = pthread_cond_timedwait(&pxsem->count_bump, &pxsem->count_lock, abstim);
     }
-    if (xresult == 0)
+    if (! xresult)
     {
         if (pxsem->count > 0)
         {
@@ -201,58 +214,43 @@ int sem_timedwait(sem_t *psem, const struct timespec *abstim)
     return 0;
 }
 
-int sem_getvalue(sem_t *sem, int *sval)
-{
-    EmbyDebug_ASSERT(sval);
-
-    int value;
-
-    // TODO: Written very fast to make things compiled. Check this.
-    EmbyDebug_ASSERT(pthread_mutex_lock(&sem->count_lock) == 0);
-
-    value = sem->count;
-
-    EmbyDebug_ASSERT(pthread_mutex_unlock(&sem->count_lock) == 0);
-
-    *sval = value;
-
-    return 0;
-}
-
 #endif
+
+
 
 namespace EmbyThreading
 {
 
     Semaphore::Semaphore()
     {
-        sem_init(&m_impl.sem, 0, 0);
+        sem_init( &m_impl, 0, 0 );
     }
 
     Semaphore::~Semaphore()
     {
-        sem_destroy(&m_impl.sem);
+        sem_destroy( &m_impl );
     }
+
 
     bool Semaphore::signal()
     {
         int value;
-        sem_getvalue(&m_impl.sem, &value);
-        sem_post(&m_impl.sem);
+        sem_getvalue( &m_impl, &value );
+        sem_post( &m_impl );
         return value == 0;
     }
 
-    bool Semaphore::wait(EmbyTime::Millis timeout)
+    bool Semaphore::wait( EmbyTime::Millis timeout )
     {
         long timeout_ms = timeout % 1000u;
         long timeout_s = timeout / 1000u;
 
         struct timespec alarmTime;
-        clock_gettime(CLOCK_REALTIME, &alarmTime);
+        clock_gettime( CLOCK_REALTIME, &alarmTime );
         alarmTime.tv_sec += timeout_s;
         alarmTime.tv_nsec += timeout_ms * 1000000;
         long carry = alarmTime.tv_nsec / 1000000000;
-        if (carry != 0)
+        if( carry != 0 )
         {
             alarmTime.tv_nsec %= 1000000000;
             alarmTime.tv_sec += carry;
@@ -260,9 +258,10 @@ namespace EmbyThreading
         int result;
         do
         {
-            result = sem_timedwait(&m_impl.sem, &alarmTime);
-        } while (result == EINTR);
-        if (result == ETIMEDOUT)
+            result = sem_timedwait( &m_impl, &alarmTime );
+        }
+        while( result == EINTR );
+        if( result == ETIMEDOUT )
         {
             return false;
         }
@@ -274,12 +273,13 @@ namespace EmbyThreading
 
     void Semaphore::signalFromInterrupt()
     {
+
     }
 
     bool Semaphore::isSomeoneWaiting() const
     {
         int value;
-        sem_getvalue(const_cast<sem_t *>(&m_impl.sem), &value);
+        sem_getvalue( const_cast<sem_t*>(&m_impl), &value );
         return value == 0;
     }
 
