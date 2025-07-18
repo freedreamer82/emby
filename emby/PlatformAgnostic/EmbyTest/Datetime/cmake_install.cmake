6e30dd175350044b60da72650319b547e2f102a0
marctsimiovalaza <marc.randriatsimiovalaza@tecniplast.it>
2025-07-18 13:56:20 +0200

    remove build cmake files

--- emby/PlatformAgnostic/EmbyTest/Datetime/cmake_install.cmake
+++ emby/PlatformAgnostic/EmbyTest/Datetime/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/mark/tp/emby
+# Install script for directory: /home/mark/tp/emby/emby/PlatformAgnostic/EmbyTest/Buffer
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -25,6 +25,11 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   else()
     set(CMAKE_INSTALL_COMPONENT)
   endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
 endif()
 
 # Is this installation the result of a crosscompile?
@@ -45,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/mark/tp/emby/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/mark/tp/emby/emby/PlatformAgnostic/EmbyTest/Buffer/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
